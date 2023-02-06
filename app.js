f06cd379f6185b282f2cd80bbac5b4495704a83b
Yuki Nagata <marin11142000@icloud.com>
2023-02-07 00:22:54 +0900

    パス修正

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const gameSelectRoutes = require('./routes/gameSelect')
 const gameCompareRoutes = require('./routes/gameCompare')
 const indexRoutes = require('./routes/index');
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static('./public'));
 app.use(express.static('./views'));
 //DB接続
 // mongoose.connect("mongodb+srv://yukinagats:abc@cluster0.cogxrva.mongodb.net/?retryWrites=true&w=majority"

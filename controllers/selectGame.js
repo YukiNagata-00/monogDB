9978cd8714e7b438df85377c77b8724b5e374693
Yuki Nagata <marin11142000@icloud.com>
2023-02-07 03:04:57 +0900

    選択ゲームのデータ受け渡し

--- controllers/selectGame.js
+++ controllers/selectGame.js
@@ -1,8 +1,8 @@
 const Food = require("../models/Food")
-
+var path = require('path');
 
 const getStart = (req, res, next) =>{
-    res.render('quiz/templates/intro.ejs');
+    res.sendFile(path.join(__dirname, '../public', 'html/select/selectStart.html'));
 }
 const getReady = (req, res, next) =>{
 
@@ -19,18 +19,47 @@ const getReady = (req, res, next) =>{
 
 
 }
-const getPlaying = (req, res, next) =>{
-    const foods = req.query.foods;
-    res.render('quiz/select/playing.ejs');
-}
-const getResult = (req, res, next) =>{
-    res.render('quiz/templates/result.ejs');
-}
+// const getPlaying = (req, res, next) =>{
+//     const foods = req.query.foods;
+//     res.render('quiz/select/playing.ejs');
+// }
+// const getPlaying = (req, res) => {
+//     try{
+//         Food.aggregate([
+//             { $sample: { size: 10 } }
+//         ], (err, foods) => {
+//             console.log(foods);
+//             res.json(foods);
+//         });
+//     }catch (err){
+//         console.log("failed")
+//     }
+//   };
+    const getPlay =  (req, res) => {
+        Food.aggregate([{ $sample: { size: 10 } }])
+        .then((questions) => {
+            console.log(questions)
+            res.json(questions);
+            //res.render('play', { questions: questions });
+            
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send('An error occured');
+        });
+    };
+    const getPlaying = (req, res, next) =>{
+        res.sendFile(path.join(__dirname, '../public', 'html/select/selectPlay.html'));
+    }
+    const getResult = (req, res, next) =>{
+        res.render('quiz/templates/result.ejs');
+    }
 
 
 module.exports = {
     getStart,
     getResult,
     getReady,
+    getPlay,
     getPlaying,
 };
--- views/quiz/select/playing.js
+++
@@ -1,4 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const foods = JSON.parse(urlParams.get('foods'));
-console.log(foods);

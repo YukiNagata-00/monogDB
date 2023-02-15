f174aafb5b791f365ee49a53867936525156f26e
Marie <muramatsumarie@gmail.com>
2023-02-15 21:39:55 +0800

    add compare game html

--- public/compare/compareStart.js
+++ public/compare/compareStart.js
@@ -1,15 +1,16 @@
-console.log("dd");
+console.log("compare start");
 
 /**
  * スタートボタンを押すと、API'/game/compare/getFood'を叩き、localStrageに取得したデータを格納。プレイ画面に遷移する。
  */
-document.querySelector('#start').addEventListener('click', function() {
+
+document.querySelector('#startC').addEventListener('click', function () {
     fetch('/game/compare/getFood')
-    .then(response => response.json())
-    .then(data => {
-        console.log('success');
-        localStorage.setItem("questions", JSON.stringify(data));
-        window.location.href = "/game/compare/play";
-    })
-    .catch(error => console.error(error));
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('success');
+            localStorage.setItem("questions", JSON.stringify(data));
+            window.location.href = "/game/compare/play";
+        })
+        .catch(error => console.error(error));
+})

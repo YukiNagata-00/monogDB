fb1041b377bbe72061cc8d502685448063ddfb4c
Yuki Nagata <marin11142000@icloud.com>
2023-02-15 22:08:28 +0900

    flashcardのapi作成

--- public/html/home.js
+++ public/html/home.js
@@ -5,5 +5,16 @@ window.onload = function () {
         console.log("select button clicked")
         window.location.href = '/game/select/start';
     })
+    
+    document.querySelector('#flashcard_start').addEventListener('click', function() {
+    fetch('/game/flashcard/getFood')
+    .then(response => response.json())
+    .then(data => {
+        console.log('success');
+        localStorage.setItem("cards", JSON.stringify(data));
+        window.location.href = "/game/flashcard/";
+    })
+    .catch(error => console.error(error));
+    });
 }
 

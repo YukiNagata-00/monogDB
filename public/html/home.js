3a3ff1450fb7dbe6068b64d93b2ffe3a0248b632
Marie <muramatsumarie@gmail.com>
2023-02-15 21:21:29 +0800

    home.js edit

--- public/html/home.js
+++ public/html/home.js
@@ -3,7 +3,7 @@ window.onload = function () {
     let select = document.getElementById("selectBtn")
     select.addEventListener('click', function () {
         console.log("select button clicked")
-        window.location.href = '../../public/select/selectStart.html';
+        window.location.href = '/game/select/start';
 
     })
 }

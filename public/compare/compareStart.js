37d2dcd2cf3e9fbb3d0e3183f07eb61f65ad1c45
Marie <muramatsumarie@gmail.com>
2023-02-16 19:16:55 +0800

    fix back button on compare start

--- public/compare/compareStart.js
+++ public/compare/compareStart.js
@@ -1,15 +1,24 @@
-console.log("dd");
-
 /**
  * スタートボタンを押すと、API'/game/compare/getFood'を叩き、localStrageに取得したデータを格納。プレイ画面に遷移する。
  */
-document.querySelector('#start').addEventListener('click', function() {
-    fetch('/game/compare/getFood')
-    .then(response => response.json())
-    .then(data => {
-        console.log('success');
-        localStorage.setItem("questions", JSON.stringify(data));
-        window.location.href = "/game/compare/play";
+
+
+window.onload = function () {
+    document.getElementById("back").addEventListener('click', function () {
+        window.location.href = '/home';
+
     })
-    .catch(error => console.error(error));
-    });
+
+
+}
+
+document.querySelector('#start').addEventListener('click', function () {
+    fetch('/game/compare/getFood')
+        .then(response => response.json())
+        .then(data => {
+            console.log('success');
+            localStorage.setItem("questions", JSON.stringify(data));
+            window.location.href = "/game/compare/play";
+        })
+        .catch(error => console.error(error));
+});

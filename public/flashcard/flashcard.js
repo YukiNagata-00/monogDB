a66530914a65dee990e8d02c439926776344811e
Aimy <aiminagasawa@gmail.com>
2023-02-27 23:49:56 +0900

    flashcardのお気に入りのハート追加、valueの名前変更、最初に表示されるのがindex0にした

--- public/flashcard/flashcard.js
+++ public/flashcard/flashcard.js
@@ -1,15 +1,20 @@
 
-
-let foodImg = document.getElementById("foodImg");
-let arrowRight = document.getElementById("arrow-right");
-let arrowLeft = document.getElementById("arrow-left");
-let index= 0;
 /**
  * このファイルを読み込むとき、LocalStrageに格納されているFoodデータをコンソールに表示する
  */
 const cards = JSON.parse(localStorage.getItem("cards"));
+let foodImg = document.getElementById("foodImg");
+let arrowRight = document.getElementById("arrow-right");
+let arrowLeft = document.getElementById("arrow-left");
+let index= 0;
+let foodName = document.getElementById("foodName");
+let ura = document.getElementById("ura");
+updateCard();
+
+console.log(cards);
 //クリックするとflip するためのcode//
 var card = document.querySelector('.card');
+
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
 });
@@ -20,14 +25,20 @@ back.addEventListener('click', function () {
 
 })
 
-let foodName = document.getElementById("foodName");
-let ura = document.getElementById("ura");
+
+
+
+function updateCard(){
+    foodName.innerText =cards[index].name ;
+    ura.innerText =cards[index].carbo ;
+    foodImg.src = '/images/foods/' + cards[index].image; 
+};
 //右矢印をクリックしたら次のカードへうつる
 arrowRight.addEventListener('click', function(){
   index ++;
   foodName.innerText =cards[index].name ;
   ura.innerText =cards[index].carbo ;
-  foodImg.src = cards[index].img;
+  foodImg.src = '/images/foods/' + cards[index].image;
 
 });
 
@@ -36,7 +47,7 @@ arrowLeft.addEventListener('click', function(){
   index --;
   foodName.innerText =cards[index].name ;
   ura.innerText =cards[index].carbo ;
-  foodImg.src = cards[index].img;
+  foodImg.src = '/images/foods/' +cards[index].image;
 
 });
 

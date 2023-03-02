eb89845b4c62a62b199a5ecde52ce1a96a4f59f5
Marie <muramatsumarie@gmail.com>
2023-03-02 22:18:41 +0800

    edit settings

---
+++ public/auth/setting.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+let username = document.getElementById('username')
+let loginEmail = document.getElementById('emailLogin')
+let scoreEmail = document.getElementById('emailScore')
+let password = document.getElementById('password')
+let confirmBtn= document.getElementById('okay')
+confirmBtn.hidden= true;
+
+
+document.getElementById("back").addEventListener('click', function () {
+    window.location.href = '/home';
+
+})
+

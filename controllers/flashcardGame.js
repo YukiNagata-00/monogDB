48fc11b78eb6e529cd18619ac32c4b7351a833e1
Aimy <aiminagasawa@gmail.com>
2023-03-03 23:52:04 +0900

    画像がはみ出ないようにした

--- controllers/flashcardGame.js
+++ controllers/flashcardGame.js
@@ -17,6 +17,15 @@ const getFood =  (req, res) => {
         res.json(food);
     });
 };
+const getAddCard =  (req, res) => {
+    AddCard.find({}).sort({ _id: 1 }).exec((err, addcard) => {
+        if (err) {
+        console.log(err);
+        res.status(500).send({ error: 'An error occurred' });
+        }
+        res.json(addcard);
+    });
+};
 const getPlay = (req, res, next) =>{
     res.sendFile(path.join(__dirname, '../public', '/flashcard/flashcard.html'));
 }
@@ -26,32 +35,33 @@ const addCardpage = (req, res, next) =>{
 };
 const addCard =
     //body('foodname').isLength({min: 1, max: 25 }).withMessage('食べ物名は１~25文字にしてください'),
-    
+  
     async(req, res) =>{
-        
+
         try{
             //ユーザーの新規作成
             const add = await AddCard.create(req.body);
-    
+            console.log("try");
+            console.log(req.body);
+        
            
         }catch(err){
             return res.status(500).json(err);
         }
     
-    }
-;
-
-
+    };
+   
 
 const updatefavorite = async(req, res) => {
+    console.log("test");
     try{
-        const id = req.body.id;
-        const foodid = req.food.id;
+        const id = req.body.userId;
+        const foodid = req.body.foodId;
         const user = await User.findById(id);
         console.log(user);
 
         User.findOneAndUpdate({id: req.body.id},
-            {$push:{favorites:Food.findById(id)}}
+            {$push:{favorites:foodid.findById(id)}}
         );
         await User.save();
         res.status(200);
@@ -68,6 +78,7 @@ module.exports = {
     getStart,
     //getReady,
     getFood,
+    getAddCard,
     getPlay,
     addCardpage,
     // getResult,

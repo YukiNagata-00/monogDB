7c96afc2a7bce5d082396d5a2270cb485fef6003
Marie <muramatsumarie@gmail.com>
2023-03-10 13:32:52 +0800

    edit settings

--- public/auth/setting.js
+++ public/auth/setting.js
@@ -4,11 +4,25 @@ let username = document.getElementById('username')
 let loginEmail = document.getElementById('emailLogin')
 let scoreEmail = document.getElementById('emailScore')
 let password = document.getElementById('password')
-let confirmBtn= document.getElementById('okay')
-
+let confirmBtn = document.getElementById('okay')
+let edit = document.getElementById('edit')
+let done = document.getElementById('done')
+let newPassword = document.getElementById('new')
+done.hidden = true;
+newPassword.hidden = true;
 
 document.getElementById("back").addEventListener('click', function () {
     window.location.href = '/home';
 
+})
+
+edit.addEventListener('click', function () {
+    done.hidden = false;
+    newPassword.hidden = false;
+})
+
+done.addEventListener('click', function () {
+    done.hidden = true;
+    newPassword.hidden = true;
 })
 

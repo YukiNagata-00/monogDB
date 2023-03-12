71f1916b3b57bc476e9ff9069b9b74173330f125
Yuki Nagata <marin11142000@icloud.com>
2023-03-12 13:18:49 +0900

    comment out firebase

--- app.js
+++ app.js
@@ -42,20 +42,20 @@ app.use('/auth', authRoutes);
 app.use('/setting', settingRoutes);
 app.use("/intro",introRoutes )
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC_2IZI-p_v5KZUdIvebshc1VJ2S-VuGV0",
-  authDomain: "carbomee.firebaseapp.com",
-  projectId: "carbomee",
-  storageBucket: "carbomee.appspot.com",
-  messagingSenderId: "783415692002",
-  appId: "1:783415692002:web:08ec31dfc61dcb7dfc2cbb"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_2IZI-p_v5KZUdIvebshc1VJ2S-VuGV0",
+//   authDomain: "carbomee.firebaseapp.com",
+//   projectId: "carbomee",
+//   storageBucket: "carbomee.appspot.com",
+//   messagingSenderId: "783415692002",
+//   appId: "1:783415692002:web:08ec31dfc61dcb7dfc2cbb"
+// };
 
-// Initialize Firebase
-const app1 = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app1 = initializeApp(firebaseConfig);

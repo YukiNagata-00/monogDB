253a7ccf28b720834098d1a31c05f0d7f4cc856f
Yuki Nagata <marin11142000@icloud.com>
2023-03-14 20:34:42 +0900

    メールAPI作成途中

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,15 @@ const { body, validationResult } = require('express-validator');
 const validation = require('../handlers/validatioin');
 const tokenHandler = require('../handlers/tokenHandler');
 const  fetchInstance  = require('./fetchInstance.js');
+const nodemailer = require('nodemailer');
+//const sendmail = require('sendmail')();
+// const sendmail = require('sendmail')({
+//     smtpPort: 25,
+//     smtpHost: 'ALT1.ASPMX.L.GOOGLE.COM',
+//     smtpTimeout: 10000, // Increase the timeout value to 10 seconds
+// });
+require('../app');
+var path = require('path');
 
 //ユーザー登録API
 const userRegister =  [
@@ -172,6 +181,7 @@ const updateUserInfo = [
     async (req, res) => {
         let currentPassword = req.body.currentPassword;
         const newPassword = req.body.newPassword;
+        let  encryptedPassword;
 
         if (currentPassword && !newPassword) {
             return res.status(400).json({ message: '新しいパスワードを入力してください' });
@@ -179,14 +189,22 @@ const updateUserInfo = [
             return res.status(400).json({ message: '現在のパスワードを入力してください' });
         }else{
             try {
-            
-                //現在のパスワードがあっているかチェック
-                    const user = await User.findById(req.body.userId);
-                    
-                    if (user) {
-                         //パスワード認証
-                        const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_KEY).toString(CryptoJS.enc.Utf8);
-                        if(currentPassword !== decryptedPassword){
+                if(currentPassword && newPassword){
+                        //現在のパスワードがあっているかチェック
+                        const user = await User.findById(req.body.userId);
+                        
+                        if (user) {
+                            //パスワード認証
+                            const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_KEY).toString(CryptoJS.enc.Utf8);
+                            if(currentPassword !== decryptedPassword){
+                                return res.status(401).json({
+                                    errors:{
+                                        param: 'password',
+                                        message: '現在のパスワードが違います'
+                                    }
+                                })
+                            }
+                        }else{
                             return res.status(401).json({
                                 errors:{
                                     param: 'password',
@@ -194,16 +212,9 @@ const updateUserInfo = [
                                 }
                             })
                         }
-                    }else{
-                        return res.status(401).json({
-                            errors:{
-                                param: 'password',
-                                message: '現在のパスワードが違います'
-                            }
-                        })
-                    }
-                // 新しいパスワードをハッシュ化
-                const encryptedPassword = CryptoJS.AES.encrypt(newPassword, process.env.PASSWORD_KEY).toString();
+                    // 新しいパスワードをハッシュ化
+                    encryptedPassword = CryptoJS.AES.encrypt(newPassword, process.env.PASSWORD_KEY).toString();
+                }
                 const updatedUser = await User.findByIdAndUpdate(req.body.userId, {
                     $set: {
                         username: req.body.username,
@@ -224,12 +235,143 @@ const updateUserInfo = [
     },
 ];
 
+//パスワード再設定メール送信API
+const sendPasswordEmail = [
+    //バリデーション
+    body('email').isEmail().withMessage('正しいメールアドレスを入力してください'),
+    body('email').custom(async (value, { req }) => {
+        try {
 
+            const user = await User.findOne({ email: value });
+            if (!user) {
+                throw new Error('ユーザーが見当たりません');
+            }
+            return true;
+        } catch (err) {
+            console.log(err)
+            throw new Error('Server Errore', err);
+        }
+    }).withMessage('ユーザーが見当たりません'),
+    validation.validate,
+    async (req, res) => {
+        try {
+            const user = await User.findOne({email: req.body.email});
+            const token = CryptoJS.HmacSHA256('sha256', process.env.PASSWORD_KEY);
+            user.passwordResetToken = token;
+            user.passwordResetExpires = Date.now() + 3600000; //1h
+            await user.save();
+
+            const passwordResetUrl =  process.env.APP_URL+'auth/show-reset-password/'+ token +'?email='+ encodeURIComponent(req.body.email);
+            console.log(passwordResetUrl)
+            
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: req.body.email,
+                    pass: process.env.MAIL_APP_PASS,
+                },
+            });
+
+            try {
+                // メール送信
+                // sendmail({
+                //     from: 'from@example.com',
+                //     to: req.body.email,
+                //     subject: 'メールのタイトルです',
+                //     text: 'メールの本文です。この例はテキストです。html形式でもOK。',
+                //     timeout: 10000
+                // }, function(err, reply) {
+                //     console.log(err && err.stack);
+                //     console.dir(reply);
+                //     console.log('send!')
+                // });
+                transporter.sendMail({
+                    from: 'from@example.com',
+                    to: req.body.email,
+                    text: "以下のURLをクリックしてパスワードを再発行してください。\n\n"+ passwordResetUrl,
+                    subject: 'パスワード再発行メール',
+                });
+                //res.json({ result: true });
+            } catch (error) {
+                console.log(error)
+                return res.status(500).json({ error: 'Failed to send email' });
+            }
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        
+    
+        return res.status(200).json({msg: "success!!"});
+    },
+];
+
+const showResetPasswordPage = async(req, res) =>{
+    const { token } = req.params;
+    try {
+        const user = await User.findOne({ passwordResetToken: token, passwordResetExpires: { $gt: Date.now() } });
+        console.log(user)
+        if (!user) {
+            return res.status(404).send('Invalid token or token expired');
+        }
+        res.sendFile(path.join(__dirname, '../public', 'auth/resetPassword.html'));
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+
+const resetPassword =  [
+    body('password').isLength({min: 5, max: 20 }).withMessage('パスワードは5~20文字にしてください'),
+    body('passwordConfirmation').isLength({min: 5, max: 20 }).withMessage('確認用パスワードは5~20文字にしてください'),
+    body('passwordConfirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password');
+        }else{
+            return true;
+        }
+    }),
+    validation.validate,
+    async (req, res) => {
+        let currentPassword = req.body.currentPassword;
+        const newPassword = req.body.newPassword;
+        let  encryptedPassword;
+
+        if (currentPassword && !newPassword) {
+            return res.status(400).json({ message: '新しいパスワードを入力してください' });
+        } else if (!currentPassword && newPassword) {
+            return res.status(400).json({ message: '現在のパスワードを入力してください' });
+        }else{
+            try {
+                
+                    // 新しいパスワードをハッシュ化
+                    encryptedPassword = CryptoJS.AES.encrypt(newPassword, process.env.PASSWORD_KEY).toString();
+                    const updatedUser = await User.findByIdAndUpdate(req.body.userId, {
+                    $set: {
+                        password: encryptedPassword,
+                        passwordResetToken: undefined,
+                        passwordResetExpires: undefined
+                    },
+                });
+                return res.status(200).json({ user: updatedUser, msg: "Password reset"});
+            } catch (err) {
+                console.log(err)
+                return res.status(500).json({ err: 'Internal server error' });
+            }
+        }
+
+    },
+];
 
 module.exports = {userRegister,
     userLogin, 
     verifyToken,
     updateLoginCount,
     updateLearningCount,
-    updateUserInfo
+    updateUserInfo,
+    sendPasswordEmail,
+    showResetPasswordPage,
+    resetPassword
 };
